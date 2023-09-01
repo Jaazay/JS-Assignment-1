5460147b8dd101f234e189467fd728c7fbe18b0a
Jaazay <jaazaybinzameer@gmail.com>
2023-09-01 22:41:35 +0500

    JS-Assignment-1

---
+++ app.js
@@ -0,0 +1,58 @@
+//! Chapter1
+
+//? Q1
+// alert("Hello World !!");
+
+//? Q2
+// alert("Error! Please enter a valid password.");
+
+//? Q3
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+//? Q4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+//? Q5
+// alert("Hello... I can run JS through my web browser's console");
+
+
+//! Chapter2
+
+//? Q1
+// var username
+
+//? Q2
+// var myName = "Jaazay Bin Zameer";
+
+//? Q3
+// var message = "Hello World";
+// alert(message);
+
+//? Q4
+// var studentName = "John Doe";
+// alert(studentName)
+// var age = "15 years old";
+// alert(age);
+// var skill = "Certified Mobile Application Development";
+// alert(skill);
+
+//? Q5
+// var food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(food);
+
+//? Q6
+// var email = "jaazaybinzameer@gmail.com";
+// alert("My email address is " + email);
+
+//? Q7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book "+ book);
+
+//? Q8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+//?Q9
+// var message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(message);
+// document.write(message);
